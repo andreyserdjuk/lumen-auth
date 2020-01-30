418310375ad81e4760cec04d570c62dd7de923f8
andreyserdjuk <ase6120@gmail.com>
2020-01-30 03:18:10 +0200

    remove pending registration after hour

--- src/Documents/RegistrationPending.php
+++ src/Documents/RegistrationPending.php
@@ -6,7 +6,7 @@ use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
 
 /**
  * @ODM\Document()
- * @ODM\Index(keys={"createdAt"=true}, options={"expireAfterSeconds"=10})
+ * @ODM\Index(keys={"createdAt"=true}, options={"expireAfterSeconds"=3600})
  */
 // todo setup delete after expiration time
 class RegistrationPending extends Registration
