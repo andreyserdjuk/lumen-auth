508c5ee107d8c65e20f53941a61f7bde1fb5d453
andreyserdjuk <ase6120@gmail.com>
2020-01-30 20:12:14 +0200

    registration pending expiration 3660 sec

--- src/Documents/RegistrationPending.php
+++ src/Documents/RegistrationPending.php
@@ -6,9 +6,98 @@ use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
 
 /**
  * @ODM\Document()
- * @ODM\Index(keys={"createdAt"=true}, options={"expireAfterSeconds"=3600})
  */
-// todo setup delete after expiration time
-class RegistrationPending extends Registration
+class RegistrationPending
 {
+    /**
+     * @var string|null
+     *
+     * @ODM\Id
+     */
+    protected $id;
+
+    /**
+     * @var string|null
+     *
+     * @ODM\Field(type="string")
+     * @ODM\UniqueIndex(order="asc")
+     */
+    protected $email;
+
+    /**
+     * @var string|null
+     *
+     * @ODM\Field(type="string")
+     */
+    protected $password;
+
+    /**
+     * @var \DateTime|null
+     *
+     * @ODM\Field(type="date")
+     * @ODM\Index(options={"expireAfterSeconds"=3600})
+     */
+    protected $createdAt;
+
+    /**
+     * @return string|null
+     */
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string|null $email
+     * @return RegistrationPending
+     */
+    public function setEmail(?string $email): RegistrationPending
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string|null $password
+     * @return RegistrationPending
+     */
+    public function setPassword(?string $password): RegistrationPending
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    /**
+     * @return \DateTime|null
+     */
+    public function getCreatedAt(): ?\DateTime
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param \DateTime|null $createdAt
+     * @return RegistrationPending
+     */
+    public function setCreatedAt(?\DateTime $createdAt): RegistrationPending
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
 }
--- src/Documents/RegistrationRequest.php
+++ src/Documents/RegistrationRequest.php
@@ -7,6 +7,97 @@ use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
 /**
  * @ODM\Document()
  */
-class RegistrationRequest extends Registration
+class RegistrationRequest
 {
+    /**
+     * @var string|null
+     *
+     * @ODM\Id
+     */
+    protected $id;
+
+    /**
+     * @var string|null
+     *
+     * @ODM\Field(type="string")
+     * @ODM\UniqueIndex(order="asc")
+     */
+    protected $email;
+
+    /**
+     * @var string|null
+     *
+     * @ODM\Field(type="string")
+     */
+    protected $password;
+
+    /**
+     * @var \DateTime|null
+     *
+     * @ODM\Field(type="date")
+     * @ODM\Index(options={"expireAfterSeconds"=3600})
+     */
+    protected $createdAt;
+
+    /**
+     * @return string|null
+     */
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string|null $email
+     * @return RegistrationRequest
+     */
+    public function setEmail(?string $email): RegistrationRequest
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string|null $password
+     * @return RegistrationRequest
+     */
+    public function setPassword(?string $password): RegistrationRequest
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    /**
+     * @return \DateTime|null
+     */
+    public function getCreatedAt(): ?\DateTime
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param \DateTime|null $createdAt
+     * @return RegistrationRequest
+     */
+    public function setCreatedAt(?\DateTime $createdAt): RegistrationRequest
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
 }
